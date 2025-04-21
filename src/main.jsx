bf3000a69267f69acacec23db0035038df976580
valapiyush <158325073+valapiyush@users.noreply.github.com>
2025-04-21 10:06:43 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/GTU-Results">
     <App />
   </BrowserRouter>
 )
