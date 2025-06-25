1891619a45bc82994789ddb3ae1293327d7d5995
valapiyush <158325073+valapiyush@users.noreply.github.com>
2025-06-25 14:52:33 +0530

    en

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/GTU-Result">
+
+  createRoot(document.getElementById('root')).render(
+  <HashRouter>
     <App />
-  </BrowserRouter>
-)
+  </HashRouter>
+
+);
